4490c72b5f2fa4f06ad67f0a3d6a82ac640b982f
alexisboutonnet <alexis.boutonnet@gmail.com>
2020-04-13 11:55:05 +0200

    responsive menu implemented
    update HTML
    update CSS
    JS creation to responsive menu actions

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
+/*STICKY MENU SCRIPT*/
 let navbar = document.getElementById('navbar');
 let header_nav_section = document.getElementById('header_nav_section');
 let main_content = document.getElementById('main_content');
-
 
 let offset_position = navbar.offsetTop;
 let lastScrollTop = 0;
@@ -18,10 +18,29 @@ window.addEventListener("scroll", function(){
 			navbar.classList.add("hidden_menu");
             navbar.classList.remove("enable_menu");
 		} else {
-			navbar.classList.remove("hiddden_menu");
 			navbar.classList.add("enable_menu");
+			navbar.classList.remove("hiddden_menu");
 		}
 	}
 	lastScrollTop = scroll_top;
 });
+
+
+/*RESPONSIVE MENU SCRIPT*/
+let mobile_nav = document.getElementById('mobile_navbar');
+let responsive_nav = document.getElementById('responsive_nav');
+
+mobile_nav.onmousedown = function(){
+	if(mobile_nav.classList.contains("navbar_close")){
+		mobile_nav.classList.add("navbar_open");
+		mobile_nav.classList.remove("navbar_close");
+		responsive_nav.style.display = "block";
+	}
+	else if(mobile_nav.classList.contains("navbar_open")){
+		mobile_nav.classList.add("navbar_close");
+		mobile_nav.classList.remove("navbar_open");
+		responsive_nav.style.display = "none";
+	}
+};
+
 	
